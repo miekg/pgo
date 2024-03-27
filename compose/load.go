51b30a595fe91771f9e00f0ed57f31eabe10c91c
Miek Gieben <miek@miek.nl>
2024-03-27 13:05:12 +0100

    Dont use pwd
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- compose/load.go
+++ compose/load.go
@@ -2,7 +2,6 @@ package compose
 
 import (
 	"context"
-	"os"
 
 	"github.com/compose-spec/compose-go/v2/cli"
 	"github.com/compose-spec/compose-go/v2/types"
@@ -10,11 +9,9 @@ import (
 
 // TODO(miek): more env stuff, env files??
 func load(file, name string, env []string) (*types.Project, error) {
-	wd, _ := os.Getwd()
-	o, err := cli.NewProjectOptions([]string{file}, cli.WithEnv(env), cli.WithWorkingDirectory(wd), cli.WithName(name))
+	o, err := cli.NewProjectOptions([]string{file}, cli.WithEnv(env), cli.WithName(name))
 	if err != nil {
 		return nil, err
 	}
-	//	o.WorkingDir, _ = os.Getwd()
 	return cli.ProjectFromOptions(context.TODO(), o)
 }
